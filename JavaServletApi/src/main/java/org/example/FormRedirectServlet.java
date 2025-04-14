2f04d733b9daa5e5c4b6dd0afaeb606954c3038f
Tamas Biro <tamas.biro@liferay.com>
2025-04-14 15:56:25 +0200

    Create my first form with Post

---
+++ JavaServletApi/src/main/java/org/example/FormRedirectServlet.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/form")
+public class FormRedirectServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/form.html");
+		dispatcher.forward(req, resp);
+	}
+}
--- JavaServletApi/src/main/java/org/example/HelloServlet.java
+++ JavaServletApi/src/main/java/org/example/HelloServlet.java
@@ -3,12 +3,25 @@ package org.example;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		response.getWriter().println("<h1>Hello from Servlet!</h1>");
+		String name = req.getParameter("name");
+		String birthdate = req.getParameter("birthdate");
+
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+
+		out.println("<html><body>");
+		out.println("<h1>Üdv, " + name + "!</h1>");
+		out.println("<p>Születési dátumod: " + birthdate + "</p>");
+		out.println("</body></html>");
+
 	}
 }
