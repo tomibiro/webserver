b96ff0b7fed44bfecc9660f05861b3d0184a8f6d
Tamas Biro <tamas.biro@liferay.com>
2025-04-14 16:17:46 +0200

    replace from forward to sendRedirect

--- JavaServletApi/src/main/java/org/example/FormRedirectServlet.java
+++ JavaServletApi/src/main/java/org/example/FormRedirectServlet.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class FormRedirectServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/form.html");
-		dispatcher.forward(req, resp);
+		resp.sendRedirect("/simple-servlet-app/form.html");
 	}
 }
